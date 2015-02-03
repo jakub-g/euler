6f2fd9337ceff3526ee97b195fbfba50d8d4b9d5
jakub-g <jakub.g.opensource@gmail.com>
2015-02-03 22:49:06 +0100

    015 cache results for perf

--- 015-lattice-paths/015.js
+++ 015-lattice-paths/015.js
@@ -1,11 +1,33 @@
 #!/usr/bin/env iojs
 
+var cache = []
+
+function setcache(m, k, val) {
+  cache[m] = cache[m] || []
+  cache[m][k] = val
+}
+
+function getcache(m, k) {
+  if (cache[m] && cache[m][k] != null)
+    return cache[m][k]
+
+  return null
+}
+
 /*
  * @return Integer number of paths in m x k lattice
  */
 function p(m, k) {
+
   if (k > m)
     return p(k, m)
+
+  var cached = getcache(m, k)
+  if (cached != null) {
+    return cached
+  }
+
+  var val = 0
 
   if (k == 0 && m == 0)
     return 0 // just in case
@@ -16,13 +38,21 @@ function p(m, k) {
   if (k == 1 && m == 1)
     return 2
 
-  if (k == 1)
-    return p(m-2, 1) + 2
+  if (k == 1) {
+    val = p(m-2, 1) + 2
+    setcache(m, k, val)
+    return val
+  }
 
-  if (m == k) // this is just optimization
-    return 2 * p(m-2, k) + 2 * p(m-1, k-1)
+  if (m == k) { // this is just optimization
+    val = 2 * p(m-2, k) + 2 * p(m-1, k-1)
+    setcache(m, k, val)
+    return val
+  }
 
-  return p(m-2, k) + p(m, k-2) + 2 * p(m-1, k-1)
+  val = p(m-2, k) + p(m, k-2) + 2 * p(m-1, k-1)
+  setcache(m, k, val)
+  return val
 }
 
 function main() {
